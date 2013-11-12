be7a44e89e03c7c93184259589bb3d564d50f66d
Steven Mirecki <steven.mirecki@gmail.com>
2013-11-12 14:40:19 -0800

    build update

--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,15 @@
-'use strict';
+angular.module('F1FeederApp.services', []).
+  factory('ergastAPIservice', function($http) {
 
-/* Services */
+    var ergastAPI = {};
 
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+    return ergastAPI;
+  });
+
